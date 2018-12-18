e12ecfa235e10a36c78d37d79fd2847b097e4225
Julian Dobmann <julian@bitcrowd.net>
2018-12-18 10:45:05 +0100

    re-enable services

--- config/environment.js
+++ config/environment.js
@@ -2,13 +2,13 @@
 const logging = require('./logging');
 // const secret = require('./secret');
 const server = require('./server');
-// const services = require('./services');
+const services = require('./services');
 const swagger = require('./swagger');
 
 module.exports = {
   logging,
   // secret,
   server,
-  // services,
+  services,
   swagger,
 };
