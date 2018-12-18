2c25b7efa50c0e065ad7df9d8e2228684ef36a26
Max Mulatz <klappradla@posteo.net>
2018-12-18 14:18:40 +0100

    [#2] Create mock endpoint for "/files" (#8)
    * Add dummy files endpoint
    
    This adds a dummy files endpoint returning just an example response and
    not taking any params.
    
    * Generate API documentation
    
    This is simply to greenify the build :v:
    
    * Change filename for README
    
    Just uppercasing the filename for the readme file - makes it easier to
    find...
    
    * Get rid of unnecessary prefix usage

---
+++ routes/files.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+
+const definitions = require('./__swagger__/definitions');
+
+const routes = [];
+
+const filesMock = [
+  {
+    file: 'Datei:Sunday 3 Besetzung (retouched).jpg',
+    url: 'https://de.wikipedia.org/wiki/Datei:Sunday_3_Besetzung_(retouched).jpg',
+  },
+];
+
+routes.push({
+  path: '/files',
+  method: 'GET',
+  options: {
+    description: 'Get all files for an article',
+    notes: 'Retrieve all files for a given article or page url.',
+    validate: {},
+    response: {
+      schema: Joi.array(),
+      status: {
+        400: definitions.errors['400'],
+      },
+    },
+    plugins: {
+      'hapi-swaggered': {
+        operationId: 'files.index',
+        security: [{ default: [] }],
+      },
+    },
+  },
+  handler: async (request, h) => h.response(filesMock),
+});
+
+module.exports = routes;
