2a1f2b8319b7b35f8c1a913bc0068f5fe0dad691
Max Mulatz <klappradla@posteo.net>
2018-12-20 11:27:06 +0100

    Add unit test for files service

---
+++ services/__fixtures__/imagesInfo.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "pages": {
+    "-1": {
+      "ns": 6,
+      "title": "File:Graphic 01.jpg",
+      "missing": "",
+      "known": "",
+      "imagerepository": "shared",
+      "imageinfo": [
+        {
+          "url": "https://upload.wikimedia.org/wikipedia/commons/e/ef/Graphic_01.jpg",
+          "descriptionurl": "https://commons.wikimedia.org/wiki/File:Graphic_01.jpg",
+          "descriptionshorturl": "https://commons.wikimedia.org/w/index.php?curid=1720256"
+        }
+      ]
+    },
+    "-2": {
+      "ns": 6,
+      "title": "File:logo.svg",
+      "missing": "",
+      "known": "",
+      "imagerepository": "shared",
+      "imageinfo": [
+        {
+          "url": "https://upload.wikimedia.org/wikipedia/commons/4/4a/logo.svg",
+          "descriptionurl": "https://commons.wikimedia.org/wiki/File:logo.svg",
+          "descriptionshorturl": "https://commons.wikimedia.org/w/index.php?curid=317966"
+        }
+      ]
+    }
+  }
+};
--- services/files.test.js
+++ services/files.test.js
@@ -1,62 +1,40 @@
 const Files = require('./files');
+
 const WikiClient = require('./wikiClient');
 const WikiUrlParser = require('./wikiUrlParser');
 
+const imageTitles = require('./__fixtures__/imageTitles');
+const imagesInfo = require('./__fixtures__/imagesInfo');
+
 jest.mock('./wikiClient');
-jest.genMockFromModule('./wikiClient');
-
 jest.mock('./wikiUrlParser');
-jest.genMockFromModule('./wikiUrlParser');
-
-const title = 'The_Hellacopters';
-const wikiUrl = 'https://de.wikipedia.org';
-
-const wikiClient = { getResultsFromApi: jest.fn() }
-WikiClient.mockImplementation(() => wikiClient);
-
-const wikiUrlParser = { parse: jest.fn() };
-WikiUrlParser.mockImplementation(() => wikiUrlParser);
-
 
 describe('Files', () => {
+  const wikiClient = { getResultsFromApi: jest.fn() };
+  const wikiUrlParser = { parse: jest.fn() };
+
+  beforeEach(() => {
+    WikiUrlParser.mockImplementation(() => wikiUrlParser);
+    WikiClient.mockImplementation(() => wikiClient);
+  });
+
   describe('getPageImages()', () => {
-    it('works', async () => {
-      const url = 'https://de.wikipedia.org/wiki/The_Hellacopters';
+    const url = 'https://en.wikipedia.org/wiki/Article_Title';
+    const title = 'Article_Title';
+    const wikiUrl = 'https://en.wikipedia.org';
 
+    it('parses the url and retrieves all images for the article', async () => {
       wikiUrlParser.parse.mockResolvedValue({ title, wikiUrl});
-      const service = new Files();
-      const subject = await service.getPageImages(url);
-      expect(wikiUrlParser.parse).toHaveBeenCalledWith(url);
+      wikiClient.getResultsFromApi.mockResolvedValueOnce(imageTitles).mockResolvedValueOnce(imagesInfo);
 
+      const service = new Files();
+      const files = await service.getPageImages(url);
+
+      expect(wikiUrlParser.parse).toHaveBeenCalledWith(url);
+      expect(wikiClient.getResultsFromApi).toHaveBeenCalledWith(title, 'images', wikiUrl);
+      expect(wikiClient.getResultsFromApi).toHaveBeenCalledWith('File:Graphic 01.jpg|File:logo.svg', 'imageinfo', wikiUrl, {iiprop: 'url'});
+
+      expect(files).toMatchSnapshot();
     });
   });
 });
-  // const service = new Files();
-  // const title = 'The_Hellacopters';
-  // const wikiUrl = 'https://de.wikipedia.org';
-  // const expectedFiles = [
-  //   {
-  //     title: 'Datei:Backyard babies 02.jpg',
-  //     url: 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Backyard_babies_02.jpg',
-  //   },
-  //   {
-  //     title: 'Datei:Commons-logo.svg',
-  //     url: 'https://upload.wikimedia.org/wikipedia/commons/4/4a/Commons-logo.svg',
-  //   },
-  //   {
-  //     title: 'Datei:ImperialStateElectric Sonisphere2010.jpg',
-  //     url:
-  //       'https://upload.wikimedia.org/wikipedia/commons/8/86/ImperialStateElectric_Sonisphere2010.jpg',
-  //   },
-  //   {
-  //     title: 'Datei:Nick royale and strings.jpg',
-  //     url: 'https://upload.wikimedia.org/wikipedia/commons/7/7b/Nick_royale_and_strings.jpg',
-  //   },
-  // ];
-
-  // it('returns all images with their name and url', async () => {
-  //   const response = await service.getPageImages(title, wikiUrl);
-
-  //   expect(response).toEqual(expectedFiles);
-  // });
-// });
