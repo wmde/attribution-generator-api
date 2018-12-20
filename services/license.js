c566708c7fdea1b20aaa3d82e315cf99d41d7d84
Julian Dobmann <julian@bitcrowd.net>
2018-12-20 15:03:42 +0100

    review changes

--- services/license.js
+++ services/license.js
@@ -4,18 +4,18 @@ function validationError(attribute) {
   return `License: Invalid "${attribute}" provided`;
 }
 
-function assertParams({ id, name, groups, compatibility, regexp, url }) {
+function validateParams({ id, name, groups, compatibility, regexp, url }) {
   assert(typeof id === 'string' && id.length > 0, validationError('id'));
-  assert(typeof name === 'string' && name.length > 0, validationError('id'));
-  assert(Array.isArray(groups), validationError('id'));
-  assert(Array.isArray(compatibility), validationError('id'));
-  assert(regexp instanceof RegExp, validationError('id'));
-  assert(!url || typeof url === 'string', validationError('id'));
+  assert(typeof name === 'string' && name.length > 0, validationError('name'));
+  assert(Array.isArray(groups), validationError('groups'));
+  assert(Array.isArray(compatibility), validationError('compatibility'));
+  assert(regexp instanceof RegExp, validationError('regexp'));
+  assert(!url || typeof url === 'string', validationError('url'));
 }
 
 class License {
   constructor(params) {
-    assertParams(params);
+    validateParams(params);
     Object.assign(this, params);
   }
 
