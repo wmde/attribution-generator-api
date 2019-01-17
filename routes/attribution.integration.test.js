f98ac58f3e668e2459458c52e64fec06ded5f836
Philipp Tessenow <philipp@tessenow.org>
2019-01-17 17:52:00 +0100

    [#9] Implement endpoint for modified attributions

--- routes/attribution.integration.test.js
+++ routes/attribution.integration.test.js
@@ -79,4 +79,80 @@ describe('attribution routes', () => {
       });
     });
   });
+
+  describe('GET /attribution/... (modified)', () => {
+    const defaults = {
+      languageCode: 'en',
+      file: 'File:Foobar.jpg',
+      typeOfUse: 'online',
+      modification: 'cropped',
+      creator: 'the great modificator',
+      licenseId: 'cc-zero',
+    };
+
+    const attribution = {
+      licenseId: 'cc-zero',
+      licenseUrl: 'https://creativecommons.org/publicdomain/zero/1.0/legalcode',
+      attributionHtml:
+        '<a href="https://commons.wikimedia.org/wiki/User:Kaldari">Kaldari</a>, <a href="https://upload.wikimedia.org/wikipedia/commons/3/3a/Foobar.jpg">Foobar</a>, cropped by the great modificator, <a href="https://creativecommons.org/publicdomain/zero/1.0/legalcode" rel="license">CC0 1.0</a>',
+      attributionPlain:
+        'Kaldari (https://upload.wikimedia.org/wikipedia/commons/3/3a/Foobar.jpg), „Foobar“, cropped by the great modificator, https://creativecommons.org/publicdomain/zero/1.0/legalcode',
+    };
+
+    function options(overrides = {}) {
+      const params = { ...defaults, ...overrides };
+      const url = `/attribution/${params.languageCode}/${params.file}/${
+        params.typeOfUse
+      }/modified/${params.modification}/${params.creator}/${params.licenseId}`;
+      return { url, method: 'GET' };
+    }
+
+    async function subject(overrides) {
+      return context.inject(options(overrides));
+    }
+
+    it('returns the attribution', async () => {
+      const response = await subject({});
+
+      expect(response.status).toBe(200);
+      expect(response.type).toBe('application/json');
+      expect(response.payload).toMatchObject(attribution);
+    });
+
+    it('returns a proper error for unknown typeOfUse', async () => {
+      const response = await subject({ typeOfUse: 'does-not-exist' });
+
+      expect(response.status).toBe(400);
+      expect(response.type).toBe('application/json');
+      expect(response.payload).toMatchObject({
+        error: 'Bad Request',
+        message: 'Invalid request params input',
+        statusCode: 400,
+      });
+    });
+
+    it('returns a proper error for weird file urls', async () => {
+      const response = await subject({ file: 'does-not-exist' });
+
+      expect(response.status).toBe(422);
+      expect(response.type).toBe('application/json');
+      expect(response.payload).toMatchObject({
+        error: 'Unprocessable Entity',
+        message: 'invalid-url',
+        statusCode: 422,
+      });
+    });
+
+    it('returns a proper error for an unknown licenseId', async () => {
+      const response = await subject({ licenseId: 'does-not-exist' });
+
+      expect(response.status).toBe(404);
+      expect(response.type).toBe('application/json');
+      expect(response.payload).toMatchObject({
+        error: 'Not Found',
+        message: 'license-not-found',
+        statusCode: 404,
+      });
+    });
+  });
 });
--- routes/attribution.js
+++ routes/attribution.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const assert = require('assert');
 
 const { knownLanguages, knownTypesOfUse, Attribution } = require('../models/attribution');
 const errors = require('../services/util/errors');
@@ -21,6 +22,8 @@ const attributionSchema = Joi.object()
 
 function handleError(h, { message }) {
   switch (message) {
+    case errors.licenseNotFound:
+      return h.error(message, { statusCode: 404 });
     case errors.invalidUrl:
     case errors.validationError:
       return h.error(message, { statusCode: 422 });
@@ -72,6 +75,76 @@ routes.push({
         languageCode,
         typeOfUse,
         fileInfo,
+      });
+      return h.response({
+        licenseId: license.id,
+        licenseUrl: license.url,
+        attributionHtml: attribution.html(),
+        attributionPlain: attribution.plainText(),
+      });
+    } catch (error) {
+      return handleError(h, error);
+    }
+  },
+});
+
+routes.push({
+  path: prefix(
+    '/{languageCode}/{file}/{typeOfUse}/modified/{modification}/{modificationAuthor}/{licenseId}'
+  ),
+  method: 'GET',
+  options: {
+    description: 'Generate attribution for a modifile work',
+    notes: 'Generate attribution hints for the given file if that file was modified.',
+    validate: {
+      params: {
+        languageCode: Joi.string().valid(knownLanguages),
+        file: Joi.string(),
+        typeOfUse: Joi.string().valid(knownTypesOfUse),
+        modification: Joi.string(),
+        modificationAuthor: Joi.string(),
+        licenseId: Joi.string(),
+      },
+    },
+    response: {
+      schema: attributionSchema,
+      status: {
+        400: definitions.errors['400'],
+        422: definitions.errors['422'],
+        500: definitions.errors['500'],
+        503: definitions.errors['503'],
+      },
+    },
+    plugins: {
+      'hapi-swaggered': {
+        operationId: 'attribution.show',
+        security: [{ default: [] }],
+      },
+    },
+  },
+  handler: async (request, h) => {
+    const { fileData, licenseStore } = request.server.app.services;
+    const {
+      languageCode,
+      file,
+      typeOfUse,
+      modification,
+      modificationAuthor,
+      licenseId,
+    } = request.params;
+    try {
+      const fileInfo = await fileData.getFileData(file);
+      const license = licenseStore.getLicenseById(licenseId);
+      assert.ok(license, errors.licenseNotFound);
+
+      const attribution = new Attribution({
+        isEdited: true,
+        license,
+        languageCode,
+        typeOfUse,
+        fileInfo,
+        modification,
+        modificationAuthor,
       });
       return h.response({
         licenseId: license.id,
