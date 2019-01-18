3fd4ba8daa32898983e609b0fd4f6a2ead5e7f38
Max Mulatz <klappradla@posteo.net>
2019-01-18 18:58:54 +0100

    Update the licence factory return value
    Update the factory to return an actual license object.

--- __helpers__/licenseFactory.js
+++ __helpers__/licenseFactory.js
@@ -1,12 +1,14 @@
-function licenseFactory({ id, name, url, groups, compatibility, regexp }) {
-  return {
-    id: id || 'cc-by-sa-3.0',
-    name: name || 'CC BY-SA 3.0',
-    groups: groups || ['cc', 'cc4'],
-    compatibility: compatibility || [],
-    regexp: regexp || /^(Bild-)?CC-BY-SA(-|\/)4.0(([^-]+.+|-migrated)*)?$/i,
-    url: url || 'https://creativecommons.org/licenses/by-sa/3.0/legalcode',
-  };
+const License = require('../models/license');
+
+function licenseFactory({
+  id = 'cc-by-sa-3.0',
+  name = 'CC BY-SA 3.0',
+  groups = ['cc', 'cc4'],
+  compatibility = [],
+  regexp = /^(Bild-)?CC-BY-SA(-|\/)4.0(([^-]+.+|-migrated)*)?$/i,
+  url = 'https://creativecommons.org/licenses/by-sa/3.0/legalcode',
+}) {
+  return new License({ id, name, url, groups, compatibility, regexp });
 }
 
 module.exports = licenseFactory;
