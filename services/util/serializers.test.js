e033df1666204616efcb7d400dded60b0e7f732e
Philipp Tessenow <philipp@tessenow.org>
2019-01-21 10:46:55 +0100

    implemented attributionSerializer

--- services/util/serializers.test.js
+++ services/util/serializers.test.js
@@ -1,5 +1,6 @@
 const serializers = require('./serializers');
 const licenseFactory = require('../../__helpers__/licenseFactory');
+const attributionFactory = require('../../__helpers__/attributionFactory');
 
 describe('license()', () => {
   const { license: serialize } = serializers;
@@ -11,8 +12,7 @@ describe('license()', () => {
   });
 
   it('serializes a license object into the specified format', () => {
-    const serialized = serialize(license);
-    expect(serialized).toEqual({
+    expect(serialize(license)).toEqual({
       code: 'cc-by-sa-4.0',
       name: 'CC BY-SA 4.0',
       url: 'https://example.org/licenses/by-sa/4.0',
@@ -21,12 +21,47 @@ describe('license()', () => {
   });
 
   it('does not fail if the object misses the required keys', () => {
-    const serialized = serialize({});
-    expect(serialized).toEqual({
+    expect(serialize({})).toEqual({
       code: undefined,
       name: undefined,
       url: undefined,
       groups: undefined,
+    });
+  });
+});
+
+describe('attribution()', () => {
+  const { attribution: serialize } = serializers;
+  const license = licenseFactory({
+    id: 'licenseId',
+    name: 'licenseName',
+    groups: [],
+    url: 'licenseUrl',
+  });
+  const attribution = attributionFactory({
+    license,
+    fileInfo: {
+      rawUrl: 'file_url',
+      title: 'file_title',
+      artistHtml: 'artist',
+    },
+  });
+
+  it('serializes a attribution object into the specified format', () => {
+    expect(serialize(attribution)).toEqual({
+      licenseId: 'licenseId',
+      licenseUrl: 'licenseUrl',
+      attributionHtml: 'artist, <a href="file_url">file_title</a>, <a href="licenseUrl" rel="license">licenseName</a>',
+      attributionPlain: 'artist (file_url), „file_title“, licenseUrl',
+    });
+  });
+
+  it('does not fail if the object misses the required keys', () => {
+    expect(serialize({})).toEqual({
+      licenseId: undefined,
+      licenseUrl: undefined,
+      attributionHtml: undefined,
+      attributionPlain: undefined,
     });
   });
 });
