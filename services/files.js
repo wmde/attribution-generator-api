a1f4df2e8d8343c020afbe5758163a8ad611db9f
Max Mulatz <klappradla@posteo.net>
2019-01-22 10:10:19 +0100

    Clean up files service
    Remove some redundant error handling and weird formatting of functions.

--- services/files.js
+++ services/files.js
@@ -1,15 +1,13 @@
-const assert = require('assert');
-
 const parseWikiUrl = require('./util/parseWikiUrl');
-const errors = require('./util/errors');
 
+function formatImageTitles(titles, page) {
+  const { images = [] } = page;
+  return [...titles, ...images.map(image => image.title)];
+}
 async function getImageTitles({ client, title, wikiUrl }) {
   const params = { imlimit: 500 };
   const { pages } = await client.getResultsFromApi([title], 'images', wikiUrl, params);
-  return Object.values(pages).reduce((acc, page) => {
-    const { images = [] } = page;
-    return [...acc, ...images.map(image => image.title)];
-  }, []);
+  return Object.values(pages).reduce(formatImageTitles, []);
 }
 
 function formatImageInfo(page) {
@@ -30,8 +28,6 @@ function formatImageInfo(page) {
 async function getImageUrls({ client, titles, wikiUrl }) {
   const params = { iiprop: 'url|size', iiurlwidth: 300 };
   const { pages } = await client.getResultsFromApi(titles, 'imageinfo', wikiUrl, params);
-  assert.ok(pages, errors.emptyResponse);
-
   return Object.values(pages).map(formatImageInfo);
 }
 
